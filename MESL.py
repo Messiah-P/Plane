3c8b291b087ccb062716cf3ff6ee168958934c28
Messiah <1661862073@qq.com>
2023-04-12 21:38:44 +0800

    request版

--- MESL.py
+++ MESL.py
@@ -1,92 +1,41 @@
+import datetime
+import re
 import requests
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
 
-from config import HEAD_BARK, LOGO_MESL, ACCOUNT, PASSWORD
+#from config import HEAD_BARK, LOGO_MESL, ACCOUNT, PASSWORD, subscribe
+subscribe = "https://in.mesl.cloud/api/v1/client/subscribe?token=b1db7f6094c8c529eaa7d923cc1d3429"
+headers = {
+    'User-Agent': 'Clash'
+}
+response = requests.get(subscribe, headers=headers)
+subscription_userinfo = response.headers['subscription-userinfo']
 
+# 解析 subscription-userinfo
+# 定义包含所有关键字和它们的值的正则表达式模式
+# (?P<upload>\d+) 表示匹配 "upload=" 后跟一或多个数字，并将这个数字组合成名为 "upload" 的命名捕获组
+pattern = r"upload=(?P<upload>\d+).*download=(?P<download>\d+).*total=(?P<total>\d+).*expire=(?P<expire>\d+)"
 
-# 点击
-def click(driver, path):
-    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, path)))
-    element.click()
+# 使用re模块中的match()方法在字符串中查找模式的匹配项
+match = re.match(pattern, subscription_userinfo)
 
-# 输入
-def send(driver, path, key):
-    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, path)))
-    element.send_keys(key)
+# 检查是否找到了匹配项
+if match:
+    # 使用group()方法获取每个命名捕获组的值
+    upload_value = int(match.group('upload'))
+    download_value = int(match.group('download'))
+    total_value = int(match.group('total'))
+    expire_value = int(match.group('expire'))
+    # 计算实际的值
+    upload_gb = round(upload_value / 1024 ** 3 , 2)
+    download_gb = round(download_value / 1024 ** 3 , 2)
+    used = round(upload_gb + download_gb , 2)
+    total_gb = round(total_value / 1024 ** 3 , 2)
+    expire_timestamp = expire_value
+    expire_date = datetime.datetime.fromtimestamp(expire_timestamp)
+    remaining_days = (expire_date - datetime.datetime.now()).days
 
-# 识别
-def text(driver, path):
-    element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, path)))
-    result = element.text
-    return result
-
-# 登陆
-def login(driver):
-    driver.get('https://in.mesl.cloud/#/login')
-    send(driver, '//*[@id="main-container"]/div[2]/div/div/div/div[1]/div/div/div[2]/input', ACCOUNT)
-    send(driver, '//*[@id="main-container"]/div[2]/div/div/div/div[1]/div/div/div[3]/input', PASSWORD)
-    click(driver, '//*[@id="main-container"]/div[2]/div/div/div/div[1]/div/div/div[4]/button')
-    sleep(2)
-
-
-# 进入控制面板
-def goto_dashboard(driver):
-    driver.get('https://in.mesl.cloud/#/dashboard')
-    lasttime = text(driver, '//*[@id="main-container"]/div/div[2]/div/div/div[2]/div/div/div/div[1]/p/span')
-    flow = driver.find_element(By.CLASS_NAME, 'font-w700').text
-    return lasttime, flow
-
-# 数据提取
-def extract_data(lasttime, flow):
-    time_left = str(lasttime).split(" ")[3] #距离到期时间
-    time = str(lasttime).split(" ")[1] #提取时间
-    time_reset = str(lasttime).split(" ")[5] #提取重置时间
-    time_y, time_m, time_d = str(time).split("/")
-
-    flow_used = float(str(flow).split(' ')[1])  # 已用流量
-    flow_used_unit = str(flow).split(' ')[2]  # 已用流量单位
-    print(flow_used)
-    flow_all = float(str(flow).split(' ')[5])  # 总流量
-    flow_all_unit = str(flow).split(' ')[6]  # 已用流量单位
-    print(flow_all)
-    flow_used_fixed = flow_used/1024 if flow_used_unit == 'MB' else flow_used # 转换单位
-    flow_left = flow_all - flow_used_fixed #剩余流量
-    flow_per = int(flow_used_fixed/flow_all*20) #使用占比
-    flow_percent = '{:.0%}'.format(flow_used_fixed/flow_all) #使用量百分比
-    print(flow_percent)
-
-    #转Bark字符串
-    flow_daily = flow_left/float(time_reset)
-    print(flow_daily)
-    flow_daily = f"剩余日均: {flow_left/float(time_reset):.2f} {flow_all_unit}"
-    flow_left = f"已用流量: {flow_used:.2f} {flow_used_unit}·{flow_all} GB"
-    time_left = f"剩余天数: {time_left}天·{time_reset}天"
-    #进度条
-    s_jd = "流量进度: ["
-    for number in range(1, flow_per):
-        s_jd = s_jd + ">"
-    for number in range(1, 20 - flow_per):
-        s_jd = s_jd + "-"
-    s_jd = s_jd + "]" + "·" + flow_percent
-
-    deadline = "截止日期: " + time_y + "年" + time_m+ "月" + time_d + "日"
-    ret = requests.get(f'{HEAD_BARK}/Mesl/{flow_daily}\n{flow_left}\n{s_jd}\n{time_left}\n{deadline}?icon={LOGO_MESL}&group=Mesl')
-
-chrome_options = Options()
-chrome_options.page_load_strategy = 'eager'
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-
-driver = webdriver.Chrome(options=chrome_options)
-login(driver)
-lasttime, flow = goto_dashboard(driver)
-extract_data(lasttime, flow)
-
-driver.close()
-driver.quit()
+    print("上传:" + str(upload_gb))
+    print("下载:" + str(download_gb))
+    print("已用流量:" + str(used))
+    print("总流量:" + str(total_gb))
+    print("剩余时间:" + str(remaining_days))
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import datetime
 import yaml
 
 # 读取YAML文件
-with open("/mnt/python/Plane/config.yml", "r") as f:
+with open("./config.yml", "r") as f:
     config = yaml.safe_load(f)
 
 # 获取Paths配置
@@ -13,6 +13,7 @@ LOG_PATH = paths["log_path"]
 links = config["links"]
 LOGO_MESL = links["logo_mesl"]
 HEAD_BARK = links["head_bark"]
+subscribe = links["subscribe"]
 
 # 账号配置
 links = config["account"]
